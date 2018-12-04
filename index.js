0469f8c60957481deb5b7da07d43fb37e26e3d2a
JOSEPH WELCH <joewelchdrums@gmail.com>
2018-12-04 07:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor((Math.random()*100)+1)
+  var obj = { itemName: [item], itemPrice: price }
+  cart.push(obj)
+  return console.log(`${item} has been added to your cart.`)
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length > 0){
+    var string = 'In your cart, you have'
+    for(var i=0; cart.length > i; i++){
+      for (var keys in cart[i]){
+        string += `${keys} at $${cart[i][keys]}`+`${cart.length === i+1 ? '.' : ', '}`
+      }
+    }
+    console.log(string)
+  }else{
+    console.log('Your shopping cart is empty.')
+  }
 }
 
 function total() {
